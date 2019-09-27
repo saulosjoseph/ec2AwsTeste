0e6a65869906087251c5bad7cb9e0ed99aa2d66f
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-09-27 15:29:09 -0300

    ec2 to test integration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', function (req, res, next) {
-    res.status(200).send({
+router.get('/teste', function (req, res, next) {
+    res.send({
         title: "Node Express API",
         version: "0.0.1"
     });
+});
+
+router.get('/ec2', function (req, res, next) {
+    res.send('EC2 Ok');
 });
 module.exports = router;
