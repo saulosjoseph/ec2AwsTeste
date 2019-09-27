19202109da231d400738bf6130cf64efafc175b4
Saulo de Sousa Joseph <saulos.joseph@gmail.com>
2019-09-27 11:33:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Rotas
 const index = require('./routes/index');
 app.use('/', index);
